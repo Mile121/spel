8bfe5a479bc37088e184933e4fd81b8393659d54
Mile121 <milevretoski@icloud.com>
2024-01-12 10:55:09 +0100

    Lagt till komentar ock fixat hur texten scrollar ut och fixat variabel namn

--- Introtext.py
+++ Introtext.py
@@ -1,11 +1,11 @@
 import time
 import os
-
+# Vi har 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def scroll_text(text, speed=0.2):
-    width = 80
+    width = 30
     x = 0
     while True:
         clear_console()
@@ -14,13 +14,12 @@ def scroll_text(text, speed=0.2):
         x += 1
         if x >= len(text):
             x = 0
+            break
 
 if __name__ == "__main__":
     text_to_scroll = (
     """
-    Välkommen till Mazebank rånet, du och din kompis är profesionella bankrånare. 
-    Erat mål är att komma in i banken utan att bli sädda. 
-    Vilken väg väljer ni?
-    """
+    Välkommen till Mazebank rånet, du och din kompis är profesionella bankrånare. Erat mål är att komma in i banken utan att bli sädda. Vilken väg väljer ni?"""
     )
     scroll_text(text_to_scroll)
+
